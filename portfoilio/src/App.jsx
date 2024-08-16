d04748244c173d02a2ea6bf6f1f133b24f8bc904
BrainGHG <120205633+jobrain1@users.noreply.github.com>
2024-08-16 17:07:51 +0200

    new file:   portfoilio/.gitignore 	new file:   portfoilio/README.md 	new file:   portfoilio/eslint.config.js 	new file:   portfoilio/index.html 	new file:   portfoilio/package-lock.json 	new file:   portfoilio/package.json 	new file:   portfoilio/postcss.config.js 	new file:   portfoilio/public/vite.svg 	new file:   portfoilio/src/App.jsx 	new file:   portfoilio/src/assets/code.jpg 	new file:   portfoilio/src/assets/logo.png 	new file:   portfoilio/src/assets/profile-pictures/user1.jpg 	new file:   portfoilio/src/assets/profile-pictures/user2.jpg 	new file:   portfoilio/src/assets/profile-pictures/user3.jpg 	new file:   portfoilio/src/assets/profile-pictures/user4.jpg 	new file:   portfoilio/src/assets/profile-pictures/user5.jpg 	new file:   portfoilio/src/assets/profile-pictures/user6.jpg 	new file:   portfoilio/src/assets/video1.mp4 	new file:   portfoilio/src/assets/video2.mp4 	new file:   portfoilio/src/components/FeatureSection.jsx 	new file:   portfoilio/src/components/Footer.jsx 	new file:   portfoilio/src/components/HeroSection.jsx 	new file:   portfoilio/src/components/Navbar.jsx 	new file:   portfoilio/src/components/Pricing.jsx 	new file:   portfoilio/src/components/Testimotionals.jsx 	new file:   portfoilio/src/components/Workflow.jsx 	new file:   portfoilio/src/constants/index.jsx 	new file:   portfoilio/src/index.css 	new file:   portfoilio/src/main.jsx 	new file:   portfoilio/tailwind.config.js 	new file:   portfoilio/vite.config.js

---
+++ portfoilio/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar"
+import HeroSection from "./components/HeroSection"
+import FeatureSection from "./components/FeatureSection"
+import Workflow from "./components/Workflow"
+import Pricing from "./components/Pricing"
+import Testimonials from "./components/Testimotionals"
+import Footer from "./components/Footer"
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <HeroSection/>
+      <FeatureSection/>
+      <Workflow/>
+      <Pricing/>
+      <Testimonials/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
